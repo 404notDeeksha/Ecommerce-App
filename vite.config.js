f3fd9f71f56a85ac46784f8d433bb4f94d6d964f
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-02-24 15:44:39 +0530

    Updated website icon to .ico

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Unfonts from "unplugin-fonts/vite";
 
-
 export default defineConfig({
-  plugins: [
-    react(),
-    Unfonts({    
-    }),
-  ],
+  plugins: [react(), Unfonts({})],
 });
