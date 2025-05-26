66285c0559e3fa85b71e5bacd18d863c08c944c6
Deeksha <sharmadeekksha@gmail.com>
2025-05-26 18:35:23 +0530

    feature - added spinner on loading in /login page

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -20,5 +20,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const {loginSuccess, logout } = authSlice.actions;
 export default authSlice.reducer;
