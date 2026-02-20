dcc843bf63a19274185fd4ff3bc0c9eea9268638
Deeksha <sharmadeekksha@gmail.com>
2026-02-20 18:01:15 +0530

    update - added config files

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      'react': reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        localStorage: 'readonly',
+        fetch: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        FormData: 'readonly',
+        File: 'readonly',
+        Blob: 'readonly',
+        navigator: 'readonly',
+        HTMLInputElement: 'readonly',
+        alert: 'readonly',
+        URLSearchParams: 'readonly',
+      },
+    },
+    settings: {
+      react: {
+        version: '18.2',
+      },
+    },
+    rules: {
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+];
