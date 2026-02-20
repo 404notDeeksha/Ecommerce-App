b56bc5953c34c89effb9804103a0c9331c0b60b9
Deeksha <sharmadeekksha@gmail.com>
2026-02-20 16:01:24 +0530

    update - added guideline for new users

--- src/pages/login/EmailAuthForm.jsx
+++ src/pages/login/EmailAuthForm.jsx
@@ -56,6 +56,9 @@ export const EmailAuthForm = () => {
             Continue
           </button>
         </form>
+        <span className="text-sm">
+          This is a demo project. Do not use real credentials.
+        </span>
       </div>
     </>
   );
